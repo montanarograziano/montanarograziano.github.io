8c6431360dbe6d243789e256199d9ebc2200dc7c
Graziano Montanaro <40232320+montanarograziano@users.noreply.github.com>
2026-08-21 19:57:00 +0200

    feat: redesign portfolio site

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -6,20 +6,11 @@ type Social = {
 
 type Presentation = {
   mail: string;
-  title: string;
-  fullName: string;
-  description: string;
   socials: Social[];
-  picture: string;
 };
 
 const presentation: Presentation = {
   mail: "graziano.montanaro98@gmail.com",
-  fullName: "Graziano Montanaro",
-  title: "Hi, I’m Graziano 👋",
-  picture: "../assets/images/profile.jpg",
-  description:
-    "I'm a passionate *Python developer* and *Machine Learning Engineer* from Italy. I'm always eager to learn and try new things. I'm a supporter of the FOSS philosophy and I'm willing to contribute to interesting projects in the open source community. Currently, I'm working as a Machine Learning Engineer at *<a title='Intella', href='https://www.intella.tech/' target='_blank'>Intella</a>*.",
   socials: [
     {
       label: "LinkedIn",
